6824d9d626e353828afb3c72cc49d378f47da9af
Daniel <30955288+dani1996dani@users.noreply.github.com>
2019-11-03 14:47:11 +0200

    changed to https so the project will work on heroku

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -1,7 +1,7 @@
 //API_KEY is stored here for the ease of testing the app if someone wants to clone the project to their PC.
 //Usually this would be in an .env file and hidden.
 const API_KEY = 'dMvTYNE5Z1EHcYDcNjheavmuUhFFO5E6';
-const HOST = 'http://dataservice.accuweather.com/';
+const HOST = 'https://dataservice.accuweather.com/';
 const FORECAST_EXPIRATION_TIME_SPAN = 600000; // 600000 ms = 10 minutes;
 
 export { 
